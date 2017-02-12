1f90a0f1144c9eded64fdf13a06aad5ab7a21a56
Gerald Bauer <gerald.bauer@gmail.com>
2017-02-12 16:14:54 +0100

    up status

--- online.rb
+++ online.rb
@@ -8,16 +8,18 @@ require 'hubba'
 ##     instead of  save_json !!!
 
 
-CACHE_DIR = '../cache'
+CACHE_DIR = '../cache'   # note: is its own repo (thus, ..)
+DATA_DIR  = './data'     # note: is just a subfolder for now inside this reop (thus, .) - stores stats/history (e.g. stars, last commit, size, etc.)
+
 
 def save_json( name, data )    ## data - hash or array
-  File.open( "#{CACHE_DIR}/#{name}.json", "w" ) do |f|
+  File.open( "#{CACHE_DIR}/#{name}.json", 'w:utf-8' ) do |f|
     f.write JSON.pretty_generate( data )
   end
 end
 
 def save_yaml( name, data )
-  File.open( "./#{name}.yml", "w" ) do |f|
+  File.open( "./#{name}.yml", 'w:utf-8' ) do |f|
     f.write data.to_yaml
   end
 end
@@ -70,8 +72,54 @@ def save_orgs
   save_yaml( "orgs", h )
 end  ## method save_repos
 
+
+####
+## up stats for all repos (in )
+##
+##  todo/check: use gitti repolist (exits?) or something -reuse, reuse, reuse!!!!
+##
+
+def update_repo_stats
+  h = YAML.load_file( "./repos.yml" )
+  pp h
+
+  org_count   = 0
+  repo_count  = 0
+  count       = 0
+
+  repos = h
+  repos.each do |key_with_counter,values|
+    repo_count += values.size
+    org_count  += 1
+  end
+
+  ## print stats
+  puts "  #{org_count} orgs, #{repo_count} repos"
+
+  gh = Hubba::Github.new( cache_dir: CACHE_DIR )
+
+  repos.each do |key_with_counter,values|
+    key = key_with_counter.sub( /\s+\([0-9]+\)/, '' )
+
+    values.each do |value|
+      full_name = "#{key}/#{value}"
+      puts "  fetching stats #{count+1}/#{repo_count} - >#{full_name}<..."
+      stats = Hubba::Stats.new( full_name )
+      stats.read( data_dir: DATA_DIR )
+      stats.fetch( gh )
+      stats.write( data_dir: DATA_DIR )
+      count += 1
+    end
+  end
+end   # method  update_repo_stats
+
+
 save_repos()
 save_orgs()
+
+### todo - ask user (gets??) - if update repo stats (yes/no) might take a while (make it optional??)
+
+update_repo_stats()
 
 
 puts "Done."
