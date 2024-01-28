ce50139f72798f27620cfa59e554a770608f68e8
Gerald Bauer <gerald.bauer@gmail.com>
2024-01-28 10:30:16 +0100

    up

--- script/helper.rb
+++ script/helper.rb
@@ -1,23 +1,5 @@
 require 'cocos'
-
-### todo/fix:
-##  move .env  loader to
-##    cocos - why? why not?
-def load_env( path='./.env' )
-  if File.exist?( path )
-     puts "==> loading .env settings..."
-     env = read_yaml( path )
-     puts "    applying .env settings... (merging into ENV)"
-     pp env
-     env.each do |k,v|
-         ENV[k] ||= v
-     end
-  end
-end
-
 load_env
-
-
 
 
 
